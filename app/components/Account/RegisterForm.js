59ebd2d22da7b432503a36eab2a8c84d5360a4cd
gabyballester <gaby.ballester@gmail.com>
2020-10-06 14:03:29 +0200

    Preparado el formulario para que almacene en estado los inputs del registro.

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -1,40 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
 
 export default function RegisterForm() {
+    // iniciamos estado con contraseña oculta
+    const [showPassword, setShowPassword] = useState(false)
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false)
+    const [formData, setFormData] = useState(defaultFormValue());
+    console.log(formData);
+
+    const onSubmit = () => {
+        console.log(formData);
+    }
+
+    const onChange = (e, type) => {
+        setFormData({ ...formData, [type]: e.nativeEvent.text })
+    }
+
     return (
         <View style={styles.formContainer}>
             <Input
                 placeholder="Correo electrónico"
                 containerStyle={styles.inputForm}
+                onChange={e => onChange(e, "email")}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name="at"
+                        iconStyle={styles.iconRight}
+                    />
+                }
             />
             <Input
                 placeholder="Contraseña"
                 containerStyle={styles.inputForm}
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={showPassword}
+                onChange={e => onChange(e, "password")}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        // Ternario para mostrar cada icono
+                        name={showPassword ? "eye-off-outline" : "eye-outline"}
+                        iconStyle={styles.iconRight}
+                        onPress={() => setShowPassword(!showPassword)}
+                    />
+                }
             />
             <Input
                 placeholder="Repetir contraseña"
                 containerStyle={styles.inputForm}
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={showRepeatPassword}
+                onChange={e => onChange(e, "reapeatPassword")}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name="eye-outline"
+                        iconStyle={styles.iconRight}
+                        onPress={() => setShowRepeatPassword(!showRepeatPassword)}
+                    />
+                }
             />
             <Button
                 title="Unirse"
                 containerStyle={styles.btnContainerRegister}
                 buttonStyle={styles.btnRegister}
+                onPress={onSubmit}
             />
         </View>
     )
 }
 
+function defaultFormValue() {
+    return {
+        email: "",
+        password: "",
+        reapeatPassword: "",
+    }
+}
+
+
 const styles = StyleSheet.create({
     formContainer: {
-        // flex: 1,
-        // alignItems: 'center',
-        // justifyContent: "center",
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: "center",
         marginTop: 30,
     },
     inputForm: {
@@ -47,5 +98,8 @@ const styles = StyleSheet.create({
     },
     btnRegister: {
         backgroundColor: "#00a680"
+    },
+    iconRight: {
+        color: "#c1c1c1"
     }
 });
