e7dce2c48e31c835c1bb2464f4a9a488c4bf09af
gabyballester <gaby.ballester@gmail.com>
2020-10-14 23:50:50 +0200

    Diseño menú creado a falta de modal de eventos

--- app/components/Account/AccountOptions.js
+++ app/components/Account/AccountOptions.js
@@ -5,8 +5,13 @@ import { map } from "lodash";
 
 export default function AccountOptions(props) {
     const { userInfo, toastRef } = props;
-    const menuOptions = generateOptions();
-    console.log(menuOptions);
+        
+    const selectedComponent = (key) => {
+        console.log("click!!");
+        console.log(key);
+    }
+
+    const menuOptions = generateOptions(selectedComponent);
 
     return (
         <View>
@@ -14,20 +19,60 @@ export default function AccountOptions(props) {
                 <ListItem
                     key={index}
                     title={menu.title}
+                    leftIcon={{
+                        type: menu.iconType,
+                        name: menu.iconNameLeft,
+                        color: menu.iconColorLeft,
+                    }}
+                    rightIcon={{
+                        type: menu.iconType,
+                        name: menu.iconNameRight,
+                        color: menu.iconColorRight,
+                    }}
+                    containerStyle={styles.menuItem}
+                    onPress={menu.onPress}
                 />
             ))}
         </View>
     )
 }
 
-function generateOptions() {
+function generateOptions(selectedComponent) {
     return [
-        { title: "Editar Nombre y Apellidos" },
-        { title: "Editar Email" },
-        { title: "Editar contraseña" },
+        {
+            title: "Editar Nombre y Apellidos",
+            iconType: "material-community",
+            iconNameLeft: "account-circle",
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "ccc",
+            onPress: () => selectedComponent("displayName")
+        },
+        {
+            title: "Editar Email",
+            iconType: "material-community",
+            iconNameLeft: "at",
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "ccc",
+            onPress: () => selectedComponent("email")
+        },
+
+        {
+            title: "Editar contraseña",
+            iconType: "material-community",
+            iconNameLeft: "lock-reset",
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "ccc",
+            onPress: () => selectedComponent("password")
+        },
     ]
 }
 
 const styles = StyleSheet.create({
-
+    menuItem: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#e3e3e3",
+    }
 })
