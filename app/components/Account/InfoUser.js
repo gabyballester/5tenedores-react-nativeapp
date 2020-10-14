fe25bbde2d5262e841cfcb53dba73d0067b0f839
gabyballester <gaby.ballester@gmail.com>
2020-10-14 18:31:14 +0200

    Desarrollada la lógica que permite abrir la galería para seleccionar archivo

--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Avatar } from "react-native-elements";
+import * as firebase from "firebase";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
 
 export default function InfoUser(props) {
-    const { userInfo, userInfo: {
-        photoURL, displayName, email
-    } } = props;
-    console.log(photoURL);
-    console.log(displayName);
-    console.log(email);
+    const { userInfo: { photoURL, displayName, email },
+        toastRef } = props;
+
+    const changeAvatar = async () => {
+        const resultPermission = await Permissions.askAsync(
+            Permissions.CAMERA_ROLL
+        );
+
+        const resultPermissionCamera = resultPermission
+            .permissions.cameraRoll.status
+
+        if (resultPermissionCamera === "denied") {
+            tostRef.current
+                .show("Es necesario aceptar los permisos de la galería");
+        } else {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [4, 3]
+            });
+            console.log(result);
+        }
+
+    }
 
     return (
         <View style={styles.viewUserInfo}>
             <Avatar
                 rounded size="large"
                 showEditButton
+                onEditPress={changeAvatar}
                 containerStyle={styles.userInfoAvatar}
                 source={
                     // Si photoURL tiene contenido
