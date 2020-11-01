a8584f9b85be63907efa15c26edbf205064d90af
gabyballester <gaby.ballester@gmail.com>
2020-11-01 21:57:35 +0100

    Recuperada la info del restaurante y guardada en estado a falta de maquetar el componente Restaurant.js

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,13 +1,32 @@
 // snippet rnfs
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+// importo firestore y lo necesario
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Restaurant(props) {
-    const {navigation, route} = props;
-    const {id, name} = route.params;
+    const { navigation, route } = props;
+    const { id, name } = route.params;
+    const [restaurant, setRestaurant] = useState(null);
+    console.log(restaurant);
 
-    navigation.setOptions({ title: name})
-    
+    navigation.setOptions({ title: name })
+
+    useEffect(() => {
+        db.collection("restaurants")
+            .doc(id)
+            .get()
+            .then((response) => {
+                const data = response.data();
+                data.id = response.id;
+                setRestaurant(data);
+            })
+    }, [])
+
     return (
         <View>
             <Text>Restaurante info</Text>
