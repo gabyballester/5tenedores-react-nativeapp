e1b16c2bd58d03ba24e06147a5f83ff88bf238cf
gabyballester <gaby.ballester@gmail.com>
2020-11-03 20:21:58 +0100

    Maquetado icono heart para agregar a favoritos, agregar favorito, quitar favorito y mostrar icono de cada estado

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,9 +1,10 @@
 // snippet rnfs
-import React, { useState, useEffect, useCallback } from 'react';
-import { StyleSheet, View, Text, ScrollView, Dimensions } from 'react-native';
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
 import { Rating, ListItem, Icon } from "react-native-elements";
 import { map } from "lodash";
 import { useFocusEffect } from "@react-navigation/native";
+import Toast from "react-native-easy-toast";
 import Loading from "../../components/Loading";
 import Carousel from "../../components/Carousel";
 import Map from "../../components/Map";
@@ -18,172 +19,246 @@ const db = firebase.firestore(firebaseApp);
 const screenWidth = Dimensions.get("window").width;
 
 export default function Restaurant(props) {
-    const { navigation, route } = props;
-    const { id, name } = route.params;
-    const [restaurant, setRestaurant] = useState(null);
-    const [rating, setRating] = useState(0)
-    console.log(restaurant);
+  const { navigation, route } = props;
+  const { id, name } = route.params;
+  const [restaurant, setRestaurant] = useState(null);
+  const [rating, setRating] = useState(0);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [userLogged, setUserLogged] = useState(false);
+  const toastRef = useRef();
 
-    navigation.setOptions({ title: name });
+  navigation.setOptions({ title: name });
+  // esto devuelve null si no está logueado
+  firebase.auth().onAuthStateChanged((user) => {
+    user ? setUserLogged(true) : setUserLogged(false);
+  });
 
-    useFocusEffect(
-        useCallback(() => {
-            db.collection("restaurants")
-                .doc(id)
-                .get()
-                .then((response) => {
-                    const data = response.data();
-                    data.id = response.id;
-                    setRestaurant(data);
-                    setRating(data.rating);
-                });
-        }, [])
-    );
+  useFocusEffect(
+    useCallback(() => {
+      db.collection("restaurants")
+        .doc(id)
+        .get()
+        .then((response) => {
+          const data = response.data();
+          data.id = response.id;
+          setRestaurant(data);
+          setRating(data.rating);
+        });
+    }, [])
+  );
 
-    if (!restaurant) return <Loading isVisible={true} text="Cargando.." />
+  useEffect(() => {
+    //si ambos tienen contenido
+    if (userLogged && restaurant) {
+      db.collection("favorites") //llama a la colección
+        //donde los ids coincidan
+        .where("idRestaurant", "==", restaurant.id)
+        .where("idUser", "==", firebase.auth().currentUser.uid)
+        .get()
+        .then((response) => {
+          //si el tamaño es 1 es que existe
+          if (response.docs.length === 1) {
+            //seteamos el favorito a true
+            setIsFavorite(true);
+          }
+        });
+    } // si uno de estos dos estados cambia
+  }, [userLogged, restaurant]);
 
-    return (
-        <ScrollView vertical style={styles.viewBody}>
-            <View style={styles.viewFavorite}>
-                <Icon
-                    type="material-community"
-                    name={"heart"}
-                    onPress={()=>console.log("add to favorites")}
-                    color="#000"
-                    underlayColor="transparent"
-                    // onPress={isFavorite ? removeFavorite : addFavorite}
-                    // color={isFavorite ? "#f00" : "#000"}
-                    // size={35}
-                    // underlayColor="transparent"
-                />
-            </View>
-            <Carousel
-                arrayImages={restaurant.images}
-                height={250}
-                width={screenWidth}
-            />
-            <TitleRestaurant
-                name={restaurant.name}
-                description={restaurant.description}
-                rating={rating}
-            />
-            <RestaurantInfo
-                location={restaurant.location}
-                name={restaurant.name}
-                address={restaurant.address}
-            />
-            <ListReviews
-                navigation={navigation}
-                idRestaurant={restaurant.id}
-            />
-        </ScrollView>
-    )
+  const addFavorite = () => {
+    if (!userLogged) {
+      toastRef.current.show(
+        "Para usar el sistema de favoritos tienes que estar logeado"
+      );
+    } else {
+      const payload = {
+        idUser: firebase.auth().currentUser.uid,
+        idRestaurant: restaurant.id,
+      };
+      db.collection("favorites")
+        .add(payload)
+        .then(() => {
+          setIsFavorite(true);
+          toastRef.current.show("Restaurante añadido a favoritos");
+        })
+        .catch(() => {
+          toastRef.current.show("Error al añadir el restaurnate a favoritos");
+        });
+    }
+  };
+
+  const removeFavorite = () => {
+    // llamo a la colección
+    db.collection("favorites") // filtro por id restaurante y user
+      .where("idRestaurant", "==", restaurant.id)
+      .where("idUser", "==", firebase.auth().currentUser.uid)
+      .get() // solicito documentos
+      .then((response) => {
+        // finalizo promesa y recorro array
+        response.forEach((doc) => {
+          const idFavorite = doc.id; // creo constante con el id
+          db.collection("favorites") // vuelvo a llamar la colección
+            .doc(idFavorite) // selecciono documento con ese id
+            .delete() //borro registro
+            .then(() => {
+              // resuelve promesa
+              setIsFavorite(false); // setea favorito a false
+              // muestra mensaje satisfactorio
+              toastRef.current.show("Restaurante eliminado de favoritos");
+            })
+            .catch(() => {
+              // si falla
+              toastRef.current.show(
+                //muestro mensaje de error
+                "Error al eliminar el restaurante de favoritos"
+              );
+            });
+        });
+      });
+  };
+
+  if (!restaurant) return <Loading isVisible={true} text="Cargando.." />;
+
+  return (
+    <ScrollView vertical style={styles.viewBody}>
+      <View style={styles.viewFavorite}>
+        <Icon
+          type="material-community"
+          name={isFavorite ? "heart" : "heart-outline"}
+          onPress={isFavorite ? removeFavorite : addFavorite}
+          color={isFavorite ? "#f00" : "#000"}
+          underlayColor="transparent"
+          // onPress={isFavorite ? removeFavorite : addFavorite}
+          // color={isFavorite ? "#f00" : "#000"}
+          // size={35}
+          // underlayColor="transparent"
+        />
+      </View>
+      <Carousel
+        arrayImages={restaurant.images}
+        height={250}
+        width={screenWidth}
+      />
+      <TitleRestaurant
+        name={restaurant.name}
+        description={restaurant.description}
+        rating={rating}
+      />
+      <RestaurantInfo
+        location={restaurant.location}
+        name={restaurant.name}
+        address={restaurant.address}
+      />
+      <ListReviews navigation={navigation} idRestaurant={restaurant.id} />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
+    </ScrollView>
+  );
 }
 
 function TitleRestaurant(props) {
-    const { name, description, rating } = props
-    return (
-        <View style={styles.viewRestaurantTitle}>
-            <View style={{ flexDirection: "row" }}>
-                <Text style={styles.nameRestaurant}>{name}</Text>
-                <Rating
-                    style={styles.rating}
-                    imageSize={20}
-                    readonly
-                    startingValue={parseFloat(rating)}
-                />
-            </View>
-            <Text style={styles.descriptionRestaurant}>{description}</Text>
-        </View>
-    )
+  const { name, description, rating } = props;
+  return (
+    <View style={styles.viewRestaurantTitle}>
+      <View style={{ flexDirection: "row" }}>
+        <Text style={styles.nameRestaurant}>{name}</Text>
+        <Rating
+          style={styles.rating}
+          imageSize={20}
+          readonly
+          startingValue={parseFloat(rating)}
+        />
+      </View>
+      <Text style={styles.descriptionRestaurant}>{description}</Text>
+    </View>
+  );
 }
 
 function RestaurantInfo(props) {
-    const { location, name, address } = props;
+  const { location, name, address } = props;
 
-    const listInfo = [
-        {
-            text: address,
-            iconName: "map-marker",
-            iconType: "material-community",
-            action: null,
-        }, {
-            text: "664411454",
-            iconName: "phone",
-            iconType: "material-community",
-            action: null,
-        },
-        {
-            text: "gabriel1@correo.es",
-            iconName: "at",
-            iconType: "material-community",
-            action: null,
-        },
-    ];
+  const listInfo = [
+    {
+      text: address,
+      iconName: "map-marker",
+      iconType: "material-community",
+      action: null,
+    },
+    {
+      text: "664411454",
+      iconName: "phone",
+      iconType: "material-community",
+      action: null,
+    },
+    {
+      text: "gabriel1@correo.es",
+      iconName: "at",
+      iconType: "material-community",
+      action: null,
+    },
+  ];
 
-
-    return (
-        <View style={styles.viewRestaurantInfo}>
-            <Text style={styles.restaurantInfoTitle}>
-                Información sobre el restaurante
-          </Text>
-            <Map location={location} name={name} height={100} />
-            {map(listInfo, (item, index) => (
-                <ListItem
-                    key={index}
-                    title={item.text}
-                    leftIcon={{
-                        name: item.iconName,
-                        type: item.iconType,
-                        color: "#00a680",
-                    }}
-                    containerStyle={styles.containerListItem}
-                />
-            ))}
-        </View>
-    );
+  return (
+    <View style={styles.viewRestaurantInfo}>
+      <Text style={styles.restaurantInfoTitle}>
+        Información sobre el restaurante
+      </Text>
+      <Map location={location} name={name} height={100} />
+      {map(listInfo, (item, index) => (
+        <ListItem
+          key={index}
+          title={item.text}
+          leftIcon={{
+            name: item.iconName,
+            type: item.iconType,
+            color: "#00a680",
+          }}
+          containerStyle={styles.containerListItem}
+        />
+      ))}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    viewBody: {
-        flex: 1,
-        backgroundColor: "#fff"
-    },
-    viewRestaurantTitle: {
-        padding: 15
-    },
-    nameRestaurant: {
-        fontSize: 20,
-        fontWeight: "bold"
-    },
-    descriptionRestaurant: {
-        marginTop: 5
-    },
-    rating: {
-        position: "absolute",
-        right: 0,
-    },
-    viewRestaurantInfo: {
-        margin: 15,
-        marginTop: 25,
-    },
-    restaurantInfoTitle: {
-        fontSize: 20,
-        fontWeight: "bold",
-        marginBottom: 10,
-    },
-    containerListItem: {
-        borderBottomColor: "#d8d8d8",
-        borderBottomWidth: 1,
-    },
-    viewFavorite: {
-        position: "absolute",
-        top: 0,
-        right: 0,
-        zIndex: 2,
-        backgroundColor: "#fff",
-        borderBottomLeftRadius: 100,
-        padding: 5,
-        paddingLeft: 15,
-    },
-})
+  viewBody: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  viewRestaurantTitle: {
+    padding: 15,
+  },
+  nameRestaurant: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  descriptionRestaurant: {
+    marginTop: 5,
+  },
+  rating: {
+    position: "absolute",
+    right: 0,
+  },
+  viewRestaurantInfo: {
+    margin: 15,
+    marginTop: 25,
+  },
+  restaurantInfoTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  containerListItem: {
+    borderBottomColor: "#d8d8d8",
+    borderBottomWidth: 1,
+  },
+  viewFavorite: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    zIndex: 2,
+    backgroundColor: "#fff",
+    borderBottomLeftRadius: 100,
+    padding: 5,
+    paddingLeft: 15,
+  },
+});
