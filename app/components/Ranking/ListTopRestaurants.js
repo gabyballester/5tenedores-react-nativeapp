454b6006748a3a9625762bd99c652e8e7a089696
gabyballester <gaby.ballester@gmail.com>
2020-11-04 11:55:50 +0100

    Obtención y listado de los 5 mejores restaurantes por puntuación, a falta de maquetar

---
+++ app/components/Ranking/ListTopRestaurants.js
@@ -0,0 +1,37 @@
+// snippet-> rnfs
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Card, Image, Icon, Rating } from "react-native-elements";
+
+export default function ListTopRestaurants(props) {
+  const { restaurants, navigation } = props;
+
+  return (
+    <FlatList
+      data={restaurants}
+      renderItem={(restaurant) => (
+        <Restaurant restaurant={restaurant} navigation={navigation} />
+      )}
+      keyExtractor={(item, index) => index.toString()}
+    />
+  );
+}
+
+function Restaurant(props) {
+  const { restaurant, navigation } = props;
+  const { id, name, rating, images, description } = restaurant.item;
+
+  return (
+    <View>
+      <Text>{name}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
--- app/screens/TopRestaurants.js
+++ app/screens/TopRestaurants.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useRef, useState, useEffect } from "react";
+import { View } from "react-native";
+import Toast from "react-native-easy-toast";
+import ListTopRestaurants from "../components/Ranking/ListTopRestaurants";
+//importaciones de firebase
+import { firebaseApp } from "../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+// constante con db
+const db = firebase.firestore(firebaseApp);
 
-export default function TopRestaurants() {
+export default function TopRestaurants(props) {
+  const { navigation } = props;
+  const [restaurants, setRestaurants] = useState([]);
+  const toastRef = useRef();
+  console.log("Listado de restaurantes");
+  console.log(restaurants);
+
+  useEffect(() => {
+    db.collection("restaurants") // llamada a restaurants
+      .orderBy("rating", "desc") //ordenado por rating
+      .limit(5) //limite 5
+      .get() // traer datos
+      //resuelvo promesa
+      .then((response) => {
+        const restaurantArray = []; // Agrego array vacío
+        // recorro response
+        response.forEach((doc) => {
+          const data = doc.data(); // guardo data en data
+          data.id = doc.id; // asigno el id doc al id data
+          restaurantArray.push(data); // voy rellenando el array con data
+        });
+        setRestaurants(restaurantArray); // actualizo estado de restaurants
+      });
+  }, []);
+
   return (
     <View>
-      <Text>TopRestaurants...</Text>
+      <ListTopRestaurants restaurants={restaurants} navigation={navigation} />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
     </View>
   );
 }
