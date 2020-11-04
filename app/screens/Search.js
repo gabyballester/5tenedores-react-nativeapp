92b6692ccac3730329fa57b965d676377733e373
gabyballester <gaby.ballester@gmail.com>
2020-11-04 13:17:48 +0100

    Haciendo consultas a firebase para traer resultados y los consoleo por ahora

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, FlatList, Image } from "react-native";
+import { SearchBar, ListItem, Icon } from "react-native-elements";
+import { FireSQL } from "firesql";
+// importación de firebase
+import firebase from "firebase/app";
 
-export default function Search() {
+// inicializo firesql e incluyo el id del restaurante en las respuesta
+const fireSQL = new FireSQL(firebase.firestore(), { includeId: "id" });
+
+export default function Search(props) {
+  const { navigation } = props;
+  const [search, setSearch] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+  console.log(restaurants);
+
+  useEffect(() => {
+    if (search) {
+      fireSQL
+        .query(`SELECT * FROM restaurants WHERE name LIKE '${search}%'`)
+        .then((response) => {
+          setRestaurants(response);
+        });
+    }
+  }, [search]);
+
   return (
     <View>
-      <Text>Search...</Text>
+      <SearchBar
+        placeholder="Busca tu restaurante..."
+        onChangeText={(e) => setSearch(e)}
+        value={search}
+        containerStyle={styles.searchBar}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  searchBar: {
+    marginBottom: 20,
+  },
+});
